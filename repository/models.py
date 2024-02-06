5ea8c6befcb021985f75bac2c5fc184797cc4055
Santiago Abad <santiagoabad@gmail.com>
2024-02-06 19:50:19 +0100

    feat: Update model to use imdb_id as primary_key

--- repository/models.py
+++ repository/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Text, JSON
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
@@ -7,7 +7,7 @@ Base = declarative_base()
 class Movie(Base):
     __tablename__ = "movies"
 
-    id = Column(Integer, primary_key=True)
+    imdb_id = Column(String, primary_key=True)
     title = Column(String)
     year = Column(String)
     rated = Column(String)
@@ -26,7 +26,6 @@ class Movie(Base):
     metascore = Column(String)
     imdb_rating = Column(String)
     imdb_votes = Column(String)
-    imdb_id = Column(String)
     type = Column(String)
     dvd = Column(String)
     box_office = Column(String)
