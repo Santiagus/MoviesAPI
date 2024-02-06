2a820d5999e8ec92865edb0f688501e396efd7d3
Santiago Abad <santiagoabad@gmail.com>
2024-02-06 12:29:58 +0100

    refactor: Delegate exception managing out of fetch_movies_data function

--- data_fetcher/data_fetcher.py
+++ data_fetcher/data_fetcher.py
@@ -23,41 +23,39 @@ def fetch_movies_data(url, parameters, headers, limit=100):
     """
     logging.info("Data fetch started")
 
-
     # Set request config
     session = Session()
-    try:
-        # Preallocate memory
-        movies_data_list = [None] * limit
-        PAGE_SIZE = 10
 
-        # Fetch movies data from specified url
-        for page in range(10):
-            parameters["page"] = page + 1
-            response = session.get(url, headers=headers, params=parameters)
-            logging.debug("Status code : ", response.status_code)
-            logging.debug("Response : ", response.text)
-            if response.status_code == 200:
-                json_data = json.loads(response.text).get("Search")                
-                movies_data_list[page * PAGE_SIZE:(page + 1) * PAGE_SIZE] = json_data[:PAGE_SIZE]
-            else:
-                logging.error(f"Error {response.status_code} : {json.loads(response.text).get('Error')}")
+    # Preallocate memory
+    movies_data_list = [None] * limit
+    PAGE_SIZE = 10
 
-        logging.info(f"Read {len(movies_data_list)} movies.")
-        logging.info("Data fetch finished")
-        return movies_data_list
+    # Fetch movies data from specified url
+    for page in range(10):
+        parameters["page"] = page + 1
+        response = session.get(url, headers=headers, params=parameters)
+        logging.debug(f"Status code : {response.status_code}")
+        logging.debug(f"Response : {response.text}")
+        response.raise_for_status()  # Raise HTTPError if status code is not successful (>= 400)
+        if response.status_code == 200:
+            json_data = json.loads(response.text).get("Search")
+            movies_data_list[page * PAGE_SIZE:(page + 1) * PAGE_SIZE] = json_data[:PAGE_SIZE]
 
-    except HTTPError as e:
-        logging.error(e)
+    logging.info(f"Read {len(movies_data_list)} movies.")
+    logging.info("Data fetch finished")
+    return movies_data_list[:limit]
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)    
+    try:
+        logging.basicConfig(level=logging.INFO)
 
-   # Load config from JSON file
-    with open('data_fetcher/fetcher_config.json', 'r') as file:
-        config = json.load(file)
+        # Load config from JSON file
+        with open('data_fetcher/fetcher_config.json', 'r') as file:
+            config = json.load(file)
 
-    # Fetch movies data with the specified config
-    movies_data = fetch_movies_data(config.get("url"), config.get("parameters"), config.get("headers"))
+        # Fetch movies data with the specified config
+        movies_data = fetch_movies_data(config.get("url"), config.get("parameters"), config.get("headers"))
+        logging.info("Program finished")
 
-    logging.info("Program finished")
+    except HTTPError as e:
+        logging.error(f"HTTPError: {e}")
