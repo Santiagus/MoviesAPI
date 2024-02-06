25c4f582fedc25f333bacda126fb7b99e5977c63
Santiago Abad <santiagoabad@gmail.com>
2024-02-06 13:41:14 +0100

    test: Add test fetch_movies_data with wrong config

--- data_fetcher/tests/test_data_fetcher.py
+++ data_fetcher/tests/test_data_fetcher.py
@@ -1,7 +1,10 @@
 import pytest
-from unittest.mock import patch, Mock
-from requests.exceptions import HTTPError
+import json
+from unittest.mock import patch, Mock, mock_open
+from requests.exceptions import HTTPError, ConnectionError
+from urllib3.exceptions import MaxRetryError, NameResolutionError
 from data_fetcher.data_fetcher import fetch_movies_data
+
 
 def test_fetch_movies_data_success():
     """
@@ -14,22 +17,23 @@ def test_fetch_movies_data_success():
         None
     """
     # Test setup
-    url = 'https://example.com'
-    parameters = {'page': 1}
-    headers = {'Accepts': 'application/json'}
+    url = "https://example.com"
+    parameters = {"page": 1}
+    headers = {"Accepts": "application/json"}
 
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.text = '{"Search": [{"Title": "Movie 1"}]}'
 
     # Patching the Session class and mocking the response
-    with patch('data_fetcher.data_fetcher.Session') as mock_session:
+    with patch("data_fetcher.data_fetcher.Session") as mock_session:
         mock_session.return_value.get.return_value = mock_response
         # Calling the function
         result = fetch_movies_data(url, parameters, headers, 1)
 
     # Assertion
     assert result == [{"Title": "Movie 1"}]
+
 
 def test_fetch_movies_data_http_error():
     """
@@ -42,18 +46,40 @@ def test_fetch_movies_data_http_error():
         None
     """
     # Test setup
-    url = 'https://example.com'
-    parameters = {'page': 1}
-    headers = {'Accepts': 'application/json'}
+    url = "https://example.com"
+    parameters = {"page": 1}
+    headers = {"Accepts": "application/json"}
 
     mock_response = Mock()
     mock_response.status_code = 404
     mock_response.raise_for_status.side_effect = HTTPError()
 
     # Patching the Session class and mocking the response
-    with patch('data_fetcher.data_fetcher.Session') as mock_session:
+    with patch("data_fetcher.data_fetcher.Session") as mock_session:
         mock_session.return_value.get.return_value = mock_response
-        
+
         # Assertion for expected HTTPError
         with pytest.raises(HTTPError):
             fetch_movies_data(url, parameters, headers)
+
+
+@patch(
+    "builtins.open",
+    new_callable=mock_open,
+    read_data='{"url": "https://example.com", "parameters": {"page": 1}, "headers": {"Accepts": "application/json"}}',
+)
+@patch(
+    "data_fetcher.data_fetcher.fetch_movies_data",
+    side_effect=[ConnectionError, MaxRetryError, NameResolutionError],
+)
+def test_fetch_movies_with_config_connection_error(mock_fetch_movies_data, mock_open):
+    # Arrange
+    # No need to mock fetch_movies_data as we're testing HTTPError handling
+
+    # Act & Assert
+    with pytest.raises(ConnectionError):
+        with open("data_fetcher/fetcher_config.json", "r") as file:
+            config = json.load(file)
+        fetch_movies_data(
+            config.get("url"), config.get("parameters"), config.get("headers")
+        )
