ee54a6e51a16f53502d2ab6694cb108b0214afbe
Santiago Abad <santiagoabad@gmail.com>
2024-02-07 23:29:32 +0100

    refactor: refactore data_fetcher code as MovieDataFetcher for easier use

--- data_fetcher/data_fetcher.py
+++ data_fetcher/data_fetcher.py
@@ -9,146 +9,122 @@ from data_layer.movies_repository import MoviesRepository
 from data_layer.models import MovieModel, Base
 
 
-def get_database_url_from_alembic_config():
-    alembic_cfg = Config("alembic.ini")
-    database_url = alembic_cfg.get_section_option("alembic", "sqlalchemy.url")
-    if database_url is None:
-        raise ValueError(
-            "Database URL is missing or could not be retrieved from Alembic configuration"
-        )
-    return database_url
+class MovieDataFetcher:
 
+    def __init__(
+        self,
+        config_file_path="data_fetcher/fetcher_config.json",
+        alembic_config="alembic.ini",
+    ):
+        self.config = self.load_config(config_file_path)
+        self.database_url = self.get_database_url_from_alembic_config(alembic_config)
+        self.engine = create_engine(self.database_url)
+        Base.metadata.create_all(self.engine)
 
-def fetch_movies_data(url, parameters, headers, limit=100):
-    """
-    Fetches movie data from the OMDB API and returns a list of movies.
+    @staticmethod
+    def load_config(config_file_path):
+        with open(config_file_path, "r") as file:
+            return json.load(file)
 
-    This function initializes a session, sends requests to the OMDB API to fetch movie data,
-    and stores the data in a list. The function logs relevant information during the process.
+    def get_database_url_from_alembic_config(self, alembic_config):
+        alembic_cfg = Config(alembic_config)
+        database_url = alembic_cfg.get_section_option("alembic", "sqlalchemy.url")
+        if database_url is None:
+            raise ValueError(
+                "Database URL is missing or could not be retrieved from Alembic configuration"
+            )
+        return database_url
 
-    Args:
-        url (str): The URL of the OMDB API.
-        parameters (dict): The parameters for the API request.
-        headers (dict): The headers for the API request.
-        limit (int, optional): The maximum number of elements to preallocate in the list.
-                              Defaults to 100.
+    @staticmethod
+    def fetch_movies_data(url, parameters, headers, limit=100):
+        logging.info("Data fetch started")
+        session = Session()
+        movies_data_list = [None] * limit
+        PAGE_SIZE = 10
+        for page in range(limit // PAGE_SIZE):
+            parameters["page"] = page + 1
+            response = session.get(url, headers=headers, params=parameters)
+            logging.debug(f"Status code : {response.status_code}")
+            logging.debug(f"Response : {response.text}")
+            response.raise_for_status()
+            if response.status_code == 200:
+                json_data = json.loads(response.text).get("Search")
+                start = page * PAGE_SIZE
+                end = (page + 1) * PAGE_SIZE
+                movies_data_list[start:end] = json_data[:PAGE_SIZE]
+                if limit <= end:
+                    break
+        logging.info(f"Read {len(movies_data_list)} movies.")
+        logging.info("Data fetch finished")
+        return movies_data_list[:limit]
 
-    Returns:
-        list: A list containing movie data fetched from the OMDB API.
-    """
-    logging.info("Data fetch started")
-
-    # Set request config
-    session = Session()
-
-    # Preallocate memory
-    movies_data_list = [None] * limit
-    PAGE_SIZE = 10
-
-    # Fetch movies data from specified url
-    for page in range(10):
-        parameters["page"] = page + 1
+    @staticmethod
+    def fetch_movie_data_by_imdb_id(url, parameters, headers, imdb_id):
+        logging.info("Data fetch started")
+        parameters["i"] = imdb_id
+        session = Session()
         response = session.get(url, headers=headers, params=parameters)
         logging.debug(f"Status code : {response.status_code}")
         logging.debug(f"Response : {response.text}")
-        response.raise_for_status()  # Raise HTTPError if status code is not successful (>= 400)
+        response.raise_for_status()
         if response.status_code == 200:
-            json_data = json.loads(response.text).get("Search")
-            # Save to corresponding indexes int list
-            start = page * PAGE_SIZE
-            end = (page + 1) * PAGE_SIZE
-            movies_data_list[start:end] = json_data[:PAGE_SIZE]
-            if limit < end:  # Stops if limit passed
-                break
+            json_data = json.loads(response.text)
+            logging.debug(f"Data: {json_data}")
+            logging.info("Data fetch finished")
+            return json_data
 
-    logging.info(f"Read {len(movies_data_list)} movies.")
-    logging.info("Data fetch finished")
-    return movies_data_list[:limit]
+    @staticmethod
+    def camel_to_snake(name):
+        import re
 
+        s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+        return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
 
-def fetch_movie_data_by_imdb_id(url, parameters, headers, imdb_id):
-    logging.info("Data fetch started")
-    parameters["i"] = imdb_id
-    session = Session()
-    response = session.get(url, headers=headers, params=parameters)
-
-    logging.debug(f"Status code : {response.status_code}")
-    logging.debug(f"Response : {response.text}")
-
-    response.raise_for_status()  # Raise HTTPError if status code is not successful (>= 400)
-
-    if response.status_code == 200:
-        json_data = json.loads(response.text)
-        logging.debug(f"Data: {json_data}")
-        logging.info("Data fetch finished")
-        return json_data
-
-
-def camel_to_snake(name):
-    """
-    Convert camelCase string to snake_case.
-    """
-    import re
-
-    s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
-    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
-
-
-if __name__ == "__main__":
-
-    try:
-        logging.basicConfig(level=logging.DEBUG)
-
-        # Load config from JSON file
-        with open("data_fetcher/fetcher_config.json", "r") as file:
-            config = json.load(file)
-
-        # Database Setup
-        database_url = get_database_url_from_alembic_config()
-        engine = create_engine(database_url)
-
-        # Create all tables defined in the models
-        Base.metadata.create_all(engine)
-
+    def fetch_and_save_movies_data(self, movie_title, limit=1):
         with UnitOfWork() as unit_of_work:
             repo = MoviesRepository(unit_of_work.session)
-
-            if repo.is_database_empty():
-                # Fetch movies data with the specified config
-                movies_data = fetch_movies_data(
-                    config.get("url"),
-                    config.get("parameters_global_search"),
-                    config.get("headers"),
-                    config.get("movies_limit"),
-                )
-
-                # Complete movie data and filter out None values
-                logging.debug(f"Updating movies data...")
-                movies_data = [
-                    fetch_movie_data_by_imdb_id(
-                        config.get("url"),
-                        config.get("parameters_featch_by_id"),
-                        config.get("headers"),
-                        movie.get("imdbID"),
-                    )
-                    for movie in movies_data
-                    if movie
-                ]
-
-                # Save into database
-                for movie in movies_data:
-                    if movie is not None:
-                        snake_case_movie = {
-                            camel_to_snake(key): value for key, value in movie.items()
-                        }
-                        existing_movie = repo.get_by_id(snake_case_movie["imdb_id"])
-                        if not existing_movie:
+            parameters_global_search = self.config.get("parameters_global_search")
+            parameters_global_search["s"] = movie_title
+            movies_data = self.fetch_movies_data(
+                self.config.get("url"),
+                parameters_global_search,
+                self.config.get("headers"),
+                limit,
+            )
+            counter = 0
+            for movie in movies_data[:limit]:
+                if movie is not None:
+                    imdb_id = movie.get("imdbID")
+                    existing_movie = repo.get_by_id(imdb_id)
+                    if not existing_movie:
+                        movie_data = self.fetch_movie_data_by_imdb_id(
+                            self.config.get("url"),
+                            self.config.get("parameters_featch_by_id"),
+                            self.config.get("headers"),
+                            imdb_id,
+                        )
+                        if movie_data:
+                            snake_case_movie = {
+                                self.camel_to_snake(key): value
+                                for key, value in movie_data.items()
+                            }
                             if "response" in snake_case_movie:
                                 del snake_case_movie["response"]
                             repo.add(MovieModel(**snake_case_movie))
-                repo.session.commit()
-            logging.debug(f"Movies in DataBase: \n {repo.get_all()}")
-            logging.info("Program finished")
+                            logging.debug(f"Added: {snake_case_movie}")
+                            counter += 1
+            repo.session.commit()
+            logging.info(f"Saved {counter} new films.")
+
+
+if __name__ == "__main__":
+    try:
+        logging.basicConfig(level=logging.DEBUG)
+        fetcher = MovieDataFetcher()
+        with UnitOfWork() as unit_of_work:
+            repo = MoviesRepository(unit_of_work.session)
+            if repo.is_database_empty():  # Requires the database to be initilized
+                fetcher.fetch_and_save_movies_data("disney", 20)
 
     except HTTPError as e:
         logging.error(f"HTTPError: {e}")
