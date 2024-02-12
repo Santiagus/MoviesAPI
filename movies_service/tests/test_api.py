d1f39012e5a27457dd9e16cf69527be8221dda2c
Santiago Abad <santiagoabad@gmail.com>
2024-02-12 09:52:27 +0100

    test: Add test_get_all_movies

---
+++ movies_service/tests/test_api.py
@@ -0,0 +1,44 @@
+import pytest
+from fastapi.testclient import TestClient
+from data_layer.tests.test_movies_repository import mock_session
+from movies_service.app import app
+from unittest.mock import MagicMock, patch
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    """Create a TestClient instance for testing the FastAPI app."""
+    return TestClient(app)
+
+
+def test_get_all_movies(test_app):
+    movies_data = [
+        {"Title": "Movie 1", "Year": "2000"},
+        {"Title": "Movie 2", "Year": "2001"},
+        {"Title": "Movie 3", "Year": "2002"},
+    ]
+
+    # Mocking the app.state.database_url
+    test_app.app.state = MagicMock()
+    test_app.app.state.database_url = "mocked_database_url"
+
+    # Mocking the UnitOfWork class
+    with patch("movies_service.api.api.UnitOfWork") as MockUnitOfWork, patch(
+        "movies_service.api.api.MoviesRepository"
+    ) as MockMoviesRepository:
+
+        # mock_unit_of_work_instance = MockUnitOfWork.return_value
+        mock_repo_instance = MockMoviesRepository.return_value
+
+        # Set up the return value of is_database_empty and get_all methods
+        mock_repo_instance.is_database_empty.return_value = False
+        mock_repo_instance.get_all.return_value = movies_data
+
+        # Make the request to the endpoint
+        response = test_app.get("/movies")
+
+        # Assert that the response is successful
+        assert response.status_code == 200
+
+        # Assert that the response body contains the expected movies data
+        assert response.json() == movies_data
