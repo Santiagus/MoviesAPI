20c5e556e1bf264eec35eed6163d19725330a23c
Santiago Abad <santiagoabad@gmail.com>
2024-02-12 10:27:29 +0100

    test: Add test_get_movie_by_mismatching_title

--- movies_service/tests/test_api.py
+++ movies_service/tests/test_api.py
@@ -71,7 +71,7 @@ def test_get_all_movies_empty_database(test_app):
 
 def test_get_movie_by_exact_title(test_app):
     movie_data = {"Title": "Movie 1", "Year": "2000"}
-    title_to_search = "Movie 1"
+
     # Mocking the app.state.database_url
     test_app.app.state = MagicMock()
     test_app.app.state.database_url = "mocked_database_url"
@@ -89,10 +89,37 @@ def test_get_movie_by_exact_title(test_app):
         mock_repo_instance.get_by_title.return_value = movie_data
 
         # Make the request to the endpoint
-        response = test_app.get("/movie/{title_to_search}")
+        response = test_app.get("/movie/title_to_search")
 
         # Assert that the response is successful
         assert response.status_code == 200
 
         # Assert that the response body contains the expected movies data
         assert response.json() == movie_data
+
+
+def test_get_movie_by_mismatching_title(test_app):
+    # Mocking the app.state.database_url
+    test_app.app.state = MagicMock()
+    test_app.app.state.database_url = "mocked_database_url"
+
+    # Mocking the UnitOfWork class
+    with patch("movies_service.api.api.UnitOfWork"), patch(
+        "movies_service.api.api.MoviesRepository"
+    ) as MockMoviesRepository:
+
+        # mock_unit_of_work_instance = MockUnitOfWork.return_value
+        mock_repo_instance = MockMoviesRepository.return_value
+
+        # Set up the return value of is_database_empty and get_all methods
+        mock_repo_instance.is_database_empty.return_value = False
+        mock_repo_instance.get_by_title.return_value = dict()
+
+        # Make the request to the endpoint
+        response = test_app.get("/movie/title_to_search")
+
+        # Assert that the response is successful
+        assert response.status_code == 404
+
+        # Assert that the response body contains the expected movies data
+        assert response.json() == {"detail": 'No exact match for "title_to_search" in the database.'}
