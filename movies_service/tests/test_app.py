0963432cb64061534ccf54011ac4fea1ad4b346d
Santiago Abad <santiagoabad@gmail.com>
2024-02-13 12:57:34 +0100

    test: Update test_lifespan_raise_exception

--- movies_service/tests/test_app.py
+++ movies_service/tests/test_app.py
@@ -47,34 +47,21 @@ async def test_lifespan():
 @pytest.mark.asyncio
 async def test_lifespan_raise_exception():
     # Mock the FastAPI app instance
-    app = MagicMock(FastAPI, create=True)
+    app = FastAPI()
 
-    # Mock the required attributes and methods of the app
-    app.state = MagicMock()
-
+    # Mock the utils.get_database_url_from_alembic_config function to raise an exception
     with patch(
         "movies_service.app.utils.get_database_url_from_alembic_config"
     ) as mock_get_database_url:
-        mock_get_database_url.return_value = "mocked_database_url"
-        with patch("movies_service.app.UnitOfWork"), patch(
-            "movies_service.app.MoviesRepository"
-        ), patch("movies_service.app.MovieDataFetcher") as mock_data_fetcher:
+        mock_get_database_url.side_effect = Exception("Database URL retrieval failed")
 
-            mock_fetch_and_save_movies_data = AsyncMock()
-            mock_fetch_and_save_movies_data.return_value = [
-                {"Title": "Movie 1", "Year": "2000"},
-                {"Title": "Movie 2", "Year": "2001"},
-                {"Title": "Movie 3", "Year": "2002"},
-            ]
-
-            # Set the mock method as an attribute of the MovieDataFetcher instance
-            mock_data_fetcher.return_value.fetch_and_save_movies_data = (
-                mock_fetch_and_save_movies_data
-            )
-
-            # Execute the lifespan context manager
+        # Use pytest.raises to assert that the exception is raised when calling lifespan
+        with pytest.raises(Exception) as exc_info:
             async with lifespan(app):
                 pass  # You can add test code here if necessary
 
-            mock_get_database_url.assert_called_once()
-            app.state.mdf.fetch_and_save_movies_data.assert_called_with("Disney")
+        # Optionally, you can assert additional details about the raised exception
+        assert str(exc_info.value) == "Database URL retrieval failed"
+
+        # Assert that the mocked function was called once
+        mock_get_database_url.assert_called_once()
